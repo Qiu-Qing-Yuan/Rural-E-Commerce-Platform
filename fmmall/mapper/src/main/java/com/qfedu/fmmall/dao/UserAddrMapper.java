6dca324474874850c027de106663a530b0b7e966
Qiu-Qingyuan <97420436+Qiu-Qingyuan@users.noreply.github.com>
2022-11-12 14:35:50 +0800

    收货地址查询业务逻辑层实现

--- fmmall/mapper/src/main/java/com/qfedu/fmmall/dao/UserAddrMapper.java
+++ fmmall/mapper/src/main/java/com/qfedu/fmmall/dao/UserAddrMapper.java
@@ -2,6 +2,8 @@ package com.qfedu.fmmall.dao;
 
 import com.qfedu.fmmall.entity.UserAddr;
 import com.qfedu.fmmall.general.GeneralDAO;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserAddrMapper extends GeneralDAO<UserAddr> {
 }
---
+++ fmmall/service/src/main/java/com/qfedu/fmmall/service/UserAddrService.java
@@ -0,0 +1,8 @@
+package com.qfedu.fmmall.service;
+
+import com.qfedu.fmmall.vo.ResultVO;
+
+public interface UserAddrService {
+
+    public ResultVO listAddrsByUid(int userId);
+}
